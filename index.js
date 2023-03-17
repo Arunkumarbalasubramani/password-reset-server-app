990caec78eca15e84ba2a0869898b48d0d2a792b
Arun Kumar <arunkumar.vbs,b@gmail.com>
2023-03-17 16:31:36 +0530

    CORS Changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 connection();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 
 const signInRoutes = require("./routes/signin");
 
@@ -15,6 +15,15 @@ app.use(signInRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Password reset app");
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 app.listen(PORT, () => {
   console.log(`Server Started and Listening to the Port - ${PORT}`);

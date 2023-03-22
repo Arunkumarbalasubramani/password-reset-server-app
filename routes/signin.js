0996c814aa2e953775c1dc3b0c10dd287644f1f0
Arun Kumar <arunkumar.vbs,b@gmail.com>
2023-03-23 00:04:32 +0530

    Final Changes in the API

--- routes/signin.js
+++ routes/signin.js
@@ -11,6 +11,53 @@ router.get("/signin", (req, res) => {
   res.send("Welcome to sign in Page");
 });
 
+//API for SignUp
+router.post("/user/signup", async (req, res) => {
+  const userName = req.body.name;
+
+  const isUser = await Users.findOne({ name: userName });
+  if (isUser) {
+    res.status(403).send({ Message: "User Exists Already. Please Sign In" });
+    return;
+  } else {
+    const passwordTobeHashed = req.body.password;
+    const hashedPassword = await generateHasedPassword(passwordTobeHashed);
+    const userData = {
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    };
+    const user = new Users(userData);
+    try {
+      const saveUser = await user.save();
+      res.status(201).send({ Message: " User SignedUp Successfully" });
+    } catch (error) {
+      res.status(403).send(`Error While Signing Up -${error}`);
+    }
+  }
+});
+
+//API for SignIn
+router.post("/user/signin", async (req, res) => {
+  try {
+    const user = await Users.findOne({ name: req.body.name });
+    if (!user) {
+      res.status(401).send({ Message: "User Not Found.Please Sign Up" });
+    } else {
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
+      if (isMatch) {
+        res.status(201).send("User Logged in successfully");
+      } else {
+        res.status(403).send("Wrong Credentials");
+      }
+    }
+  } catch (error) {
+    res.status(404).send(`Error While Sigining In -${error}`);
+  }
+});
+
+//API For Generating Password reset token
+
 router.post("/user/passwordreset", async (req, res) => {
   try {
     const userData = await Users.findOne({ email: req.body.email });
@@ -63,70 +110,7 @@ router.post("/user/passwordreset", async (req, res) => {
   }
 });
 
-router.post("/user/signup", async (req, res) => {
-  const userName = req.body.name;
-
-  const isUser = await Users.findOne({ name: userName });
-  if (isUser) {
-    res.status(403).send({ Message: "User Exists Already. Please Sign In" });
-    return;
-  } else {
-    const passwordTobeHashed = req.body.password;
-    const hashedPassword = await generateHasedPassword(passwordTobeHashed);
-    const userData = {
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-    };
-    const user = new Users(userData);
-    try {
-      const saveUser = await user.save();
-      res.status(201).send({ Message: " User SignedUp Successfully" });
-    } catch (error) {
-      res.status(403).send(`Error While Signing Up -${error}`);
-    }
-  }
-});
-
-router.post("/user/signin", async (req, res) => {
-  try {
-    const user = await Users.findOne({ name: req.body.name });
-    if (!user) {
-      res.status(401).send({ Message: "User Not Found.Please Sign Up" });
-    } else {
-      const isMatch = await bcrypt.compare(req.body.password, user.password);
-      if (isMatch) {
-        res.status(201).send("User Logged in successfully");
-      } else {
-        res.status(403).send("Wrong Credentials");
-      }
-    }
-  } catch (error) {
-    res.status(404).send(`Error While Sigining In -${error}`);
-  }
-});
-
-// router.get("/reset-password/:id/:token", async (req, res) => {
-//   try {
-//     const { id, token } = req.params;
-//     const userData = await Users.findById(id);
-//     const secret = process.env.MY_SECRET_KEY + userData.password;
-//     if (!userData) {
-//       res.status(404).send("User Does Not Exist");
-//     } else {
-//       const verifyToken = jwt.verify(token, secret);
-//       if (verifyToken) {
-//         const viewPath = path.join(__dirname, "../views/index.ejs");
-//         res.render(viewPath, { id, token });
-//       } else {
-//         res.status(403).send({ Message: "Not Verified " });
-//       }
-//     }
-//   } catch (error) {
-//     res.status(500).send(`Error While Connecting to Server- ${error}`);
-//   }
-// });
-
+//API to Change Password
 router.post("/reset-password/:id/:token", async (req, res) => {
   try {
     const { id, token } = req.params;
